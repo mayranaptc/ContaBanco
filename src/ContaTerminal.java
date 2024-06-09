adb6512efff09d80d4c316a336b30b26a9956000
Mayra Araújo <134660113+mayranaptc@users.noreply.github.com>
2024-06-09 14:21:01 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
-public class App {
+import java.util.Scanner;
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int conta = 0;
+        String agencia = " ";
+        String nomeClinte = " Mario Andrade ";
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta : (Exemplo:1023 - depois ENTER para o próximo campo.)");
+        conta = scanner.nextInt();
+        System.out.println(
+                "Por favor, digite o número da Agência : (Exemplo:102-3 - depois ENTER para o próximo campo.)");
+        agencia = scanner.next();
+
+        System.out.println("Olá " + nomeClinte + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + conta + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
